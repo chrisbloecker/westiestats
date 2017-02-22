f2fcf051474145eec61ba8629fedfa3e88d9380d
Christopher Blöcker <chrisbloecker@googlemail.com>
2017-02-22 09:30:42 +0800

    fixing wrong file path

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # remove the possibly still existing lock file for the database
-rm -f /headjudge/state/Database/open.lock
+rm -f /westiestats/state/Database/open.lock
 
 /westiestats/westiestats
